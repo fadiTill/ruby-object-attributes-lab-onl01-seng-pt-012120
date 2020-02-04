b21bd1ea4e6dd4820ffd77680716dbaf2432fb6a
fadimata tillman <fadi.tillman@gmail.com>
2020-02-04 21:15:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,3 @@
 
 class Dog
-
-  def initialize(writes,read)
-@writes =  writes
-@reads = read
-
-def name =
-  @writes =  write the name of the dog
 end
-
- def name
- @reads = reads the name of the dog
- end
-
-Dog.new(writes,read)
